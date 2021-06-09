2a02b003b2d86793dfd2220055e9bd1786b02488
AlejoJorgeGonzalez <alejandro.jorgeg1206@gmail.com>
2021-06-09 10:45:01 -0500

    24 Recorriendo un string con for

---
+++ 24_recorrer.py
@@ -0,0 +1,14 @@
+# Programa para recorrer una cadena de caracteres
+
+def run():
+    # nombre = input('Escribe tu nombre: ')
+    # for letra in nombre:
+    #     print(letra)
+
+    frase = input('Escribe una frase: ')
+    for caracter in frase:
+        print(caracter.upper())
+
+
+if __name__ == '__main__':
+    run()
